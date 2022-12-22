52ff9ba81b33c9229c6b746ce3c8ae17aacab87c
Lilian Zhao <milanzq309@gmail.com>
2022-12-21 22:01:24 -0800

    fixxed annlatin and added new regexes

--- contentScript.js
+++ contentScript.js
@@ -1,4 +1,4 @@
-const elements = document.getElementsByTagName('*');
+/**const elements = document.getElementsByTagName('*');
 const vowels = ['a', 'e', 'i', 'o', 'u']
 
 for (var i = 0; i < elements.length; i++) {
@@ -9,24 +9,28 @@ for (var i = 0; i < elements.length; i++) {
 
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-            var replacedText = text.replace(/[aeiou]/gi, 'ann');
+            var replacedText = text.replace(/a/gi, 'ann');
 
             if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node);
             }
         }
     }
-}
-/** 
+} */
+
 const text = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, caption, span, a')
-const vowels = ['a', 'e', 'i', 'o', 'u']
+
+/*
+for (let i = 0; i < text.length; i++) {
+    text[i].innerHTML = text[i].innerHTML.replace(/[aeiou]/gi, 'ann')
+}
 
 for (let i = 0; i < text.length; i++) {
-    for (let j = 0; j < vowels.length; j++) {
-        let vowel = vowels[j]
-        if (text[i].innerHTML.toLowerCase.includes(vowel)) {
-            text[i].innerHTML = text[i].innerHTML.replace(vowel, 'ann');
-        }
-    }
+    text[i].innerHTML = 'Never gonna give you up'
+}**/
+
+for (let i = 0; i < text.length; i++) {
+    text[i].innerHTML = text[i].innerHTML.replace(/(?<= is )(.*)(?=(.|,|\\?))/gi, 'never gonna give you up')
+    text[i].innerHTML = text[i].innerHTML.replace(/(?<= to )(.*)(?=(.|,|\\?))/gi, 'never let you down')
+    text[i].innerHTML = text[i].innerHTML.replace(/(?<= of )(.*)(?=(.|,|\\?))/gi, 'never running around and deserting you')
 }
-**/
--- popup.js
+++ popup.js
@@ -1,12 +1,4 @@
 // adding a new bookmark row to the popup
-const addNewBookmark = () => {};
-
-const viewBookmarks = () => {};
-
-const onPlay = e => {};
-
-const onDelete = e => {};
-
-const setBookmarkAttributes =  () => {};
+const toggle = () => {};
 
 document.addEventListener("DOMContentLoaded", () => {});
