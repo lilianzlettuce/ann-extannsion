74b543b9d92580b2e34624fe15310bd8212be558
Lilian Zhao <milanzq309@gmail.com>
2022-12-21 23:33:45 -0800

    edited re

--- contentScript.js
+++ contentScript.js
@@ -1,23 +1,3 @@
-/**const elements = document.getElementsByTagName('*');
-const vowels = ['a', 'e', 'i', 'o', 'u']
-
-for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
-
-    for (var j = 0; j < element.childNodes.length; j++) {
-        var node = element.childNodes[j];
-
-        if (node.nodeType === 3) {
-            var text = node.nodeValue;
-            var replacedText = text.replace(/a/gi, 'ann');
-
-            if (replacedText !== text) {
-                element.replaceChild(document.createTextNode(replacedText), node);
-            }
-        }
-    }
-} */
-
 const text = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, caption, span, a')
 
 /*
@@ -30,7 +10,13 @@ for (let i = 0; i < text.length; i++) {
 }**/
 
 for (let i = 0; i < text.length; i++) {
-    text[i].innerHTML = text[i].innerHTML.replace(/(?<= is )(.*)(?=(.|,|\\?))/gi, 'never gonna give you up')
-    text[i].innerHTML = text[i].innerHTML.replace(/(?<= to )(.*)(?=(.|,|\\?))/gi, 'never let you down')
-    text[i].innerHTML = text[i].innerHTML.replace(/(?<= of )(.*)(?=(.|,|\\?))/gi, 'never running around and deserting you')
+    text[i].innerHTML = text[i].innerHTML.replace(/(?<= is | was )(.*)(?=(.|,))/gi, 'never gonna give you up')
+    text[i].innerHTML = text[i].innerHTML.replace(/(?<= to )(.*)(?=(.|,))/gi, 'never let you down')
+    text[i].innerHTML = text[i].innerHTML.replace(/(?<= of )(.*)(?=(.|,))/gi, 'never running around and deserting you')
+}
+
+const images = document.querySelectorAll('img')
+
+for (let i = 0; i < images.length; i++) {
+    images[i].src = 'https://pbs.twimg.com/profile_images/1326707048478892033/Ln0v50LP_400x400.jpg'
 }
